35a1fecd2addaa5efd7702f7356cd17b77c5e19a
Anto <antothomas31300@gmail.com>
2024-11-26 20:51:37 +0530

    first commit

---
+++ learn/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    code = models.CharField(max_length=10, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Price of the course
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="subjects")
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    text = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="questions")
+    
+    def __str__(self):
+        return f"Question: {self.text[:50]}... - Subject: {self.subject.name}"
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+    text = models.TextField()
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Answer: {self.text[:50]}... - {'Correct' if self.is_correct else 'Incorrect'}"
+
+    
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)  # Approval status for login
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, related_name='staff')  # Course assigned to staff
+    subjects = models.ManyToManyField(Subject, related_name='staff')  # Subjects assigned to staff
+
+    def __str__(self):
+        return f"{self.user.username} - {'Approved' if self.approved else 'Pending Approval'}"
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)
+    enrollment_number = models.CharField(max_length=100, unique=True)
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True, blank=True, related_name='students')
+    purchased_courses = models.ManyToManyField('Course', related_name='purchasers', blank=True)  # Courses purchased by the student
+
+    def __str__(self):
+        return f"{self.user.username} - {'Approved' if self.approved else 'Pending Approval'}"
+
+
+
+
